a04a1c1696533b9f5e407c118b675aecc143d7a9
chris98122 <chris98122@126.com>
2019-12-25 14:53:03 +0800

    UI framework

---
+++ detectIon_with_UI/util.py
@@ -0,0 +1,14 @@
+
+import hashlib
+import re
+import sys
+from PyQt5 import  QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import * 
+
+import copy
+import numpy as np
+
+
+from PIL import Image   
